b5a562172c077e9961efc4ad4b34ef2ee56edef7
Christopher Kennedy-Nuñez <yo@git.furioursus.dev>
2026-06-22 23:31:06 -0400

    feat: add mail obfuscation to button contact
    - Adds a feature to allow email text to be hidden via JavaScript

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import { defineConfig } from "astro/config";
 import icon from "@twodft/astro-icon";
 import mdx from "@astrojs/mdx";
 
+import mailObfuscation from "astro-mail-obfuscation";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), mdx()],
+  integrations: [icon(), mailObfuscation(), mdx()],
 });
